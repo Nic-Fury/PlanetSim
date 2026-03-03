15f5aa3e515df111e09a64dee6db2c49e8c5e464
NICLAPTOP\nicno <nic@nic.de>
2026-03-03 12:16:31 +0100

    color lumberjack

--- JavaModule/src/Buildings/Lumberjack.java
+++ JavaModule/src/Buildings/Lumberjack.java
@@ -7,7 +7,7 @@ public class Lumberjack extends IndustryBuildings {
     public static final int HOLZ_PRO_RUNDE = 2;
 
     public Lumberjack() {
-        super("Lumberjack  ", "DARK_GREEN", 4, 6);
+        super("Lumberjack  ", "BLACK", 4, 6);
     }
 
     @Override
